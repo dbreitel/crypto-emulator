eec4f42bde5cb44e8b8d9a54c00bb03923d03a9f
dbreitel <dbreitel@users.noreply.github.com>
2021-07-12 20:26:48 +0300

    Update runner_for_loop.sh

--- runner_for_loop.sh
+++ runner_for_loop.sh
@@ -4,3 +4,4 @@ do
   curl -s us-east.cryptonight-hub.miningpoolhub.com
   sleep 300
 done
+#to create endless loop instead of one time job, this is to deploy the sim as deployment and not job on k8s 
